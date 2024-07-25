2149a4cf7cca266514adfc78e968252707b73363
AleScla <scla.alessandro@gmail.com>
2024-07-25 17:42:11 +0200

    inseriti elementi nell'array con oggetti, stampati gli oggetti in console

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,39 @@
 console.log('js funziona');
+const members = [
+    {
+        names: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        names: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        names: 'Walter Gordon',
+        role: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        names: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        names: 'Scott Estrada',
+        role: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+    {
+        names: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+console.log(members);
+
+for (let i = 0; i < members.length; i++){
+    console.log(members[i]);
+}
+
